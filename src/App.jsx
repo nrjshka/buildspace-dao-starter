58fc4c280165175d1f8a03994585320f66617c11
Max Korolev <nrjshka@gmail.com>
2022-07-07 20:00:23 +0300

    mm auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+
 const App = () => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+
+  console.log('address', address)
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to NarutoDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
       <h1>Welcome to My DAO</h1>
